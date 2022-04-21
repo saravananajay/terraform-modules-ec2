858bb20a7b62787b68d981767948415c37bdb29c
saravananajay <34689245+saravananajay@users.noreply.github.com>
2022-04-21 11:44:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,11 @@ variable "ami_id" {}
 variable "region" {
   default = "ap-south-1"
 }
-variable "instance_type" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+/* variable "instance_type" {} */
 /* variable "tag" {
 default="Hello-Module"
 } */
